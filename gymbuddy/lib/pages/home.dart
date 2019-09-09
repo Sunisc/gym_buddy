1876c25fe94ec3244543c8dd7211e24bbe34889f
ethanwu18 <33034670+ethanwu18@users.noreply.github.com>
2019-09-09 09:35:26 -0400

    Update home.dart

--- gymbuddy/lib/pages/home.dart
+++ gymbuddy/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:gymbuddy/pages/setup/welcome.dart';
 
 class Home extends StatelessWidget {
   const Home({Key key, @required this.user}) : super(key: key);
@@ -8,21 +9,92 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home ${user.email}')
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: Firestore.instance.collection('users').document(user.uid).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else if (snapshot.hasData) {
-            return checkRole(snapshot.data);
-          }
-          return LinearProgressIndicator();
-        },
-      ),
+
+      return new Scaffold(
+        body: new Stack(
+          
+      children: <Widget>[
+        
+        ClipPath(
+          child: Container(color: Colors.lightBlue.withOpacity(0.8)),
+          clipper: getClipper(),
+        ),
+        Positioned(
+            width: MediaQuery.of(context).size.width,
+            top: MediaQuery.of(context).size.height / 5,
+            child: Column(
+              children: <Widget>[
+                Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://media.licdn.com/dms/image/C5603AQEm-t7Y2gy_iw/profile-displayphoto-shrink_100_100/0?e=1573084800&v=beta&t=TvJ3hqc_iwVGK7ZG7Ass9QSWKYZKsl6iF-jGg2WqMHY'),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                        boxShadow: [
+                          BoxShadow(blurRadius: 7.0, color: Colors.black)
+                        ])),
+                SizedBox(height: 90.0),
+                Text(
+                  'Ethan Wu',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  'Age: 20',
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(height: 25.0),
+                Container(
+                    height: 30.0,
+                    width: 95.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.greenAccent,
+                      color: Colors.green,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Center(
+                          child: Text(
+                            'Edit Name',
+                            style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+                          ),
+                        ),
+                      ),
+                    )),
+                    SizedBox(height: 25.0),
+                Container(
+                    height: 30.0,
+                    width: 95.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.redAccent,
+                      color: Colors.red,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                        onTap: () => {},
+                        child: Center(
+                          child: Text(
+                            'Log out',
+                            style: TextStyle(color: Colors.white, fontFamily: 'Montserrat'),
+                          ),
+                        ),
+                      ),
+                    ))
+              ],
+            ))
+      ],
+    )
     );
   }
 
@@ -48,3 +120,21 @@ class Home extends StatelessWidget {
     return Center(child: Text(snapshot.data['name']));
   }
 }
+  class getClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+  
+  }
